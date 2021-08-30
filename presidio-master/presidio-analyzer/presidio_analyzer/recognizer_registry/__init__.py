e8ccd12a6d1d6999f46a10e7a5e05519d2663775
Yspyridis <io.spridis@gmail.com>
2021-08-30 13:45:26 +0300

    remove presidio

--- presidio-master/presidio-analyzer/presidio_analyzer/recognizer_registry/__init__.py
+++
@@ -1,4 +0,0 @@
-from .recognizers_store_api import RecognizerStoreApi
-from .recognizer_registry import RecognizerRegistry
-
-__all__ = ["RecognizerStoreApi", "RecognizerRegistry"]
--- presidio-master/presidio-analyzer/tests/mocks/__init__.py
+++
@@ -1,4 +0,0 @@
-from .nlp_engine_mock import NlpEngineMock
-from .app_tracer_mock import AppTracerMock
-
-__all__ = ["NlpEngineMock", "AppTracerMock"]

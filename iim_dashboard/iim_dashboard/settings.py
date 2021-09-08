2acd4e713375ac5f1935e979ddf966fd5b00fb16
Yannis <io.spyridis@gmail.com>
2021-09-08 15:56:15 +0300

    revert settings

--- iim_dashboard/iim_dashboard/settings.py
+++ iim_dashboard/iim_dashboard/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+# import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -153,10 +153,10 @@ LOGIN_REDIRECT_URL = 'dashboard:index'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
+# STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
 
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR, 'media/')
+# MEDIA_URL='/media/'
+# MEDIA_ROOT=os.path.join(BASE_DIR, 'media/')
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'dashboard/static/'),

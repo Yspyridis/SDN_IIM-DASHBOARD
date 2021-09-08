34c46ef0cfe97db92ca8c074ebf46492cc77e607
Yannis <io.spyridis@gmail.com>
2021-09-08 13:52:10 +0300

    settings

--- iim_dashboard/iim_dashboard/settings.py
+++ iim_dashboard/iim_dashboard/settings.py
@@ -152,6 +152,10 @@ LOGIN_REDIRECT_URL = 'dashboard:index'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media/')
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'dashboard/static/'),

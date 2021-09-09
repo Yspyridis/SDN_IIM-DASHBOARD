1a86e29a9d5732b16daddb4b791c6eb359d804fa
Yspyridis <io.spridis@gmail.com>
2021-09-09 09:21:06 +0300

    test allo hosts

--- iim_dashboard/iim_dashboard/settings.py
+++ iim_dashboard/iim_dashboard/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = '((06qd(5c0*=5()*hjn+iqesz)3man#u9x)x^i3m9o93i2)ikk'
 DEBUG = False
 
 # The first IP is to check API on local network and should be removed
-ALLOWED_HOSTS = ['mlst-iim-server.eurodyn.com', '10.250.100.60', '192.168.1.161', '192.168.1.150','localhost', '127.0.0.1']
-# ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['mlst-iim-server.eurodyn.com', '10.250.100.60', '192.168.1.161', '192.168.1.150','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

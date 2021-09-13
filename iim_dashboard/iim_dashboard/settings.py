0a18675d7196d7cc8ff67c89b9fdc46adbe6b911
Yspyridis <io.spridis@gmail.com>
2021-09-13 20:43:10 +0300

    settings for development

--- iim_dashboard/iim_dashboard/settings.py
+++ iim_dashboard/iim_dashboard/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '((06qd(5c0*=5()*hjn+iqesz)3man#u9x)x^i3m9o93i2)ikk'
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = '((06qd(5c0*=5()*hjn+iqesz)3man#u9x)x^i3m9o93i2)ikk'
+# SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True

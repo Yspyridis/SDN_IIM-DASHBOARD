d8334c869e7f5bee4b2584993be56fb7a3a7c441
Yspyridis <io.spridis@gmail.com>
2021-09-16 11:24:33 +0300

    test production settings to allow loading of external js

--- iim_dashboard/iim_dashboard/settings.py
+++ iim_dashboard/iim_dashboard/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     # corsheaders maybe needs to be more down, possibly right above CommonMiddleware
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+    # 'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

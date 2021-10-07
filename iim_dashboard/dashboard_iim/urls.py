bef43f918b2d15336dc470400b350c6373e96ae2
root <root@mlst-iim-server.eurodyn.com>
2021-10-07 14:06:03 +0300

    rename dashboard_iim

---
+++ iim_dashboard/dashboard_iim/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url, include
+from . import views
+from django.views.generic import RedirectView
+
+# from django.urls import path
+
+app_name = 'dashboard_iim'
+
+urlpatterns = [
+    url('^$', views.index, name='index'),
+    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/assets/favicon.ico')),
+
+    url('/compare', views.compare, name='compare'),
+]
+ 
---
+++ iim_dashboard/dashboard_iim/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponse
+from django.template import loader
+
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def index(request):
+
+    context = {
+    'app_name': "dashboard_iim",
+    }
+    template = loader.get_template('dashboard/index.html')
+    return HttpResponse(template.render(context, request))
+
+@login_required
+def compare(request):
+
+    context = {
+    'app_name': "dashboard_iim",
+    'page_name': "compare",
+    }
+    template = loader.get_template('dashboard_iim/compare.html')
+    return HttpResponse(template.render(context, request))
+    

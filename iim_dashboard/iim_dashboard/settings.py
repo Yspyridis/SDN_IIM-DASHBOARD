5cdc49509a23b0d8a59ae948ac213efe6461e446
Yannis <io.spyridis@gmail.com>
2022-05-26 14:12:25 +0300

    1

--- iim_dashboard/iim_dashboard/settings.py
+++ iim_dashboard/iim_dashboard/settings.py
@@ -162,8 +162,8 @@ LOGIN_REDIRECT_URL = 'dashboard_iim:index'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT=os.path.join(BASE_DIR, '..dashboard_iim/static/')
-STATIC_ROOT=os.path.join(BASE_DIR, 'dashboard_iim/static/')
+STATIC_ROOT=os.path.join(BASE_DIR, '..dashboard_iim/static/')
+# STATIC_ROOT=os.path.join(BASE_DIR, 'dashboard_iim/static/')
 
 print('STATIC_ROOT: ', STATIC_ROOT)
 
